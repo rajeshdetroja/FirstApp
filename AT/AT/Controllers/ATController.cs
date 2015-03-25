5845e31eefe02ce9fe5d65cd1db88a6fd5e938ce
rajeshdetroja <rajudetroja@outlook.com>
2015-03-25 17:17:44 +0530

    New AT
    New AT

---
+++ AT/AT/Controllers/ATController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AT.Controllers
+{
+    public class ATController : Controller
+    {
+        //
+        // GET: /AT/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string AT()
+        {
+            return "First AT";
+        }
+
+    }
+}
