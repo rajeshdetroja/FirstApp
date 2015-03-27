ab57b0b14be5385aee3f237d5dc688dcffe7f34d
rajeshdetroja <rajudetroja@outlook.com>
2015-03-27 17:08:26 +0530

    add new
    add new

---
+++ TestFomLocal/UnitTestFomLocal/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFomLocal
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string r = string.Empty;
+
+            Assert.AreEqual(string.Empty, r);
+        }
+    }
+}
