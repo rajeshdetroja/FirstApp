30484d821eae4cef1c537b22e55839a85d6f8778
rajeshdetroja <rajudetroja@outlook.com>
2015-03-27 18:17:11 +0530

    6:17
    6:17

--- TestFomLocal/UnitTestFomLocal/UnitTest1.cs
+++ TestFomLocal/UnitTestFomLocal/UnitTest1.cs
@@ -13,5 +13,29 @@ namespace UnitTestFomLocal
 
             Assert.AreEqual(string.Empty, r);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string r = "Raju";
+
+            Assert.AreEqual("Raju", r);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string r = null;
+
+            Assert.AreEqual(null, r);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string r = string.Empty;
+
+            Assert.AreEqual("Null", r);
+        }
     }
 }
