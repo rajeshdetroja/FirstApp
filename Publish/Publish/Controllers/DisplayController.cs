3b260d1769b19d4d2101e9104e450fa4a9e7adba
rajeshdetroja <rajudetroja@outlook.com>
2015-04-01 12:20:17 +0530

    new

---
+++ Publish/Publish/Controllers/DisplayController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Publish.Controllers
+{
+    public class DisplayController : Controller
+    {
+        //
+        // GET: /Display/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Display()
+        {
+            return "Hello Good Morning";
+        }
+
+    }
+}
