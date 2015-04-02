12287c48d73158bbdfecd39948de5d244241221e
rajeshdetroja <rajudetroja@outlook.com>
2015-04-02 10:18:35 +0530

    add new
    add new

---
+++ Publish_NuGet/Publish_NuGet/Controllers/DispalyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Publish_NuGet.Controllers
+{
+    public class DispalyController : Controller
+    {
+        //
+        // GET: /Dispaly/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Display()
+        {
+            return "hello... ";
+        }
+    }
+}
